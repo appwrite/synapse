5bfe7cf07946967f3bf4ab6363ef45aded2b725f
Chirag Aggarwal <chiragaggarwal5k@gmail.com>
2025-06-09 06:28:03 +0000

    chore: export embeddings

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,9 @@
+export { Appwrite } from "./services/appwrite";
 export { Code } from "./services/code";
+export { Embeddings } from "./services/embeddings";
 export { Filesystem } from "./services/filesystem";
 export { Git } from "./services/git";
 export { System } from "./services/system";
 export { Terminal } from "./services/terminal";
 export { Synapse } from "./synapse";
-export { Appwrite } from "./services/appwrite";
+
