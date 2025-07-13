315945b100ac13bc66e02eff7a96a82a2d0fca84
Chirag Aggarwal <chiragaggarwal5k@gmail.com>
2025-07-13 17:19:02 +0530

    fix: test build

--- tsup.test.config.ts
+++ tsup.test.config.ts
@@ -4,7 +4,7 @@ import { defineConfig, type Options } from "tsup";
 const commonConfig: Options = {
   sourcemap: true,
   clean: true,
-  dts: true,
+  dts: false,
   treeshake: true,
   target: "node16",
   entry: ["src/**/*.ts", "tests/**/*.ts"],
